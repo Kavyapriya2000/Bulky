a3d155845546e6f29c64c57361f41f2997c02881
kavya <kavyapriya0806@gmail.com>
2023-08-18 15:43:32 +0530

    Add project files.

---
+++ BookSpot.DataAccess/Repository/ApplicationUserRepository.cs
@@ -0,0 +1,24 @@
+﻿using BookSpot.DataAccess.Data;
+using BookSpot.DataAccess.Repository.IRepository;
+using BookSpot.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookSpot.DataAccess.Repository
+{
+    public class ApplicationUserRepository : Repository<ApplicationUser>, IApplicationUserRepository
+    {
+        private ApplicationDbContext _db;
+        public ApplicationUserRepository(ApplicationDbContext db):base(db)
+        {
+            _db = db;
+        }
+
+     
+
+    }
+}
